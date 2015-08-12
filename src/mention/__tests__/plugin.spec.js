c5b899f5f3a62c9edba4e8951c928668382c0f7f
Eric Kong <eric.kong98@gmail.com>
2015-08-12 12:57:52 -0400

    started working on tests for plugin.js for @mentions

--- src/mention/__tests__/plugin.spec.js
+++ src/mention/__tests__/plugin.spec.js
@@ -45,12 +45,69 @@ describe('TinyMCE Plugin', () => {
     editor = null;
   });
 
+  // _testFunction
   it('should be tested', () => {
     expect(getState().dataSource[0]).toEqual('alex gray');
   });
 
-  it('should be near mention', () => {
+  // _isNearMention
+  it('should be near mentions from the editor', () => {
     const str = 'Hello there @jim and @john';
     expect(_isNearMention(str).toString()).toEqual('@john');
-  })
+  });
+
+  // _removeMentionFromEditor
+  it('should remove mention from the Editor', () => {
+    store.dispatch(query('eric'));
+    store.dispatch(select());
+
+    var mentionNode = document.createElement('strong');
+    const node = document.createTextNode('@eric kong');
+    mentionNode.appendChild(node);
+    expect(_removeMentionFromEditor(mentionNode)).toEqual('eric kong');
+
+    store.dispatch(query('tim'));
+    store.dispatch(select());
+
+    var mentionNode2 = document.createElement('p');
+    const node2 = document.createTextNode('@timothy meaney');
+    mentionNode2.appendChild(node2);
+    expect(_removeMentionFromEditor(mentionNode2)).toEqual('timothy meaney');
+  });
+
+  // _handleEditorBackspace
+  it('should handle backspace presses & reset the current query', () => {
+
+    var mentionNode = document.createElement('strong');
+    const node = document.createTextNode('@eric kong');
+    mentionNode.appendChild(node);
+    mentionNode.className = 'mention';
+    editor.selection = mentionNode;
+
+    editor.setContent(mentionNode.innerHTML);
+    console.log(editor.getContent());
+
+    editor.selection.getNode = function(){
+      return editor.selection;
+    };
+    // console.log(editor.selection);
+    _handleEditorBackspace({keyCode: 8});
+
+    expect(editor.getContent()).toEqual('');
+  });
+
+  // _performIntermediateActions
+  // it('should validate key-presses and checks for intermediate actions', () => {
+  //   store.dispatch(query('eric'));
+  //   store.dispatch(_performIntermediateActions(13, event));
+  //   expect(getState().query).toEqual('eric');
+  //
+  // });
+
+  // _handleKeyPress
+  // it('should parse input & dispatch queries from internal key-presses', (done) => {
+  //
+  //
+  //   done();
+  // });
 });
